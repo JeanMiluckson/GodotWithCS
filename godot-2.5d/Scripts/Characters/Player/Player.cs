625a6ba2f66f2e0d37ed8c3fd3244a85d21f14f2
JNUKI <webhelpjean@gmail.com>
2025-11-15 00:24:58 -0300

    Update Player.cs

--- godot-2.5d/Scripts/Characters/Player/Player.cs
+++ godot-2.5d/Scripts/Characters/Player/Player.cs
@@ -10,6 +10,7 @@ public partial class Player : CharacterBody3D
 	public override void _Ready()
 	{
 		animPlayerNode.Play(GConstants.ANIM_IDLE);
+		GD.Print("Ainda testando");
 	}
 
 	public override void _PhysicsProcess(double delta)
